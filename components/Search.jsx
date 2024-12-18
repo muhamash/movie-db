605445d7d85590f39b65daba11c63d73f6bea4cb
muhamsh <muhammad-ashraful@outlook.com>
2024-12-18 16:27:37 +0600

    loading on search

--- components/Search.jsx
+++ components/Search.jsx
@@ -7,7 +7,8 @@ import { useCallback, useState } from "react";
 export default function Search() {
     const [searchQuery, setSearchQuery] = useState("");
     const [searchResults, setSearchResults] = useState([]);
-    const [errorState, setErrorState] = useState(null); 
+    const [errorState, setErrorState] = useState(null);
+    const [isLoading, setIsLoading] = useState(false); 
 
     const debounce = (func, delay) => {
         let timer;
@@ -20,8 +21,10 @@ export default function Search() {
     const fetchSearchResults = async (query) => {
         try {
             setErrorState(null);
+            setIsLoading(true); 
             if (!query.trim()) {
                 setSearchResults([]);
+                setIsLoading(false);
                 return;
             }
 
@@ -32,6 +35,8 @@ export default function Search() {
         } catch (error) {
             console.error("Error fetching search results:", error);
             setErrorState("Failed to fetch results. Please try again later.");
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -54,59 +59,75 @@ export default function Search() {
                 id="searchInput"
                 placeholder="Search movies..."
                 className="bg-black bg-opacity-50 text-white px-4 py-2 rounded border border-gray-600 focus:outline-none focus:border-white"
-                value={ searchQuery }
-                onChange={ handleInputChange }
+                value={searchQuery}
+                onChange={handleInputChange}
             />
             <AnimatePresence mode="wait">
-                { searchResults && !errorState && (
+                {/* Loading Indicator */}
+                {isLoading && (
+                    <motion.div
+                        className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.2, ease: "easeInOut" }}
+                    >
+                        <div className="loader w-6 h-6 border-4 border-yellow-500 border-t-transparent rounded-full animate-spin"></div>
+                    </motion.div>
+                )}
+
+                {/* Search Results */}
+                {searchResults && !errorState && !isLoading && (
                     <motion.div
                         id="searchResults"
                         className="absolute w-full mt-2 bg-slate-800 bg-opacity-90 rounded-lg backdrop-blur-md"
-                        initial={ { opacity: 0, y: -10 } }
-                        animate={ { opacity: 1, y: 0 } }
-                        exit={ { opacity: 0, y: -10 } }
-                        transition={ { duration: 0.3, ease: "easeOut" } }
+                        initial={{ opacity: 0, y: -10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -10 }}
+                        transition={{ duration: 0.3, ease: "easeOut" }}
                     >
                         <ul className="max-h-[200px] overflow-y-auto">
-                            { searchResults.map( ( movie ) => (
+                            {searchResults.map((movie) => (
                                 <motion.li
-                                    key={ movie.id }
+                                    key={movie.id}
                                     className="px-4 py-2 text-yellow-500 font-semibold cursor-pointer hover:shadow-sm hover:bg-yellow-600 hover:text-white"
-                                    initial={ { opacity: 0, x: -10 } }
-                                    animate={ { opacity: 1, x: 0 } }
-                                    exit={ { opacity: 0, x: 10 } }
-                                    transition={ { duration: 0.2, ease: "easeOut" } }
+                                    initial={{ opacity: 0, x: -10 }}
+                                    animate={{ opacity: 1, x: 0 }}
+                                    exit={{ opacity: 0, x: 10 }}
+                                    transition={{ duration: 0.2, ease: "easeOut" }}
                                 >
-                                    { movie.title }
+                                    {movie.title}
                                 </motion.li>
-                            ) ) }
-                            {
-                                searchQuery && searchResults.length === 0 && (
-                                    <motion.li
-                                        initial={ { opacity: 0, x: -10 } }
-                                        animate={ { opacity: 1, x: 0 } }
-                                        exit={ { opacity: 0, x: 10 } }
-                                        transition={ { duration: 0.2, ease: "easeOut" } } className="text-yellow-400 text-md p-3 font-bold font-mono">
-                                        NO match found
-                                    </motion.li>
-                                )
-                            }
+                            ))}
+                            {searchQuery && searchResults.length === 0 && (
+                                <motion.li
+                                    initial={{ opacity: 0, x: -10 }}
+                                    animate={{ opacity: 1, x: 0 }}
+                                    exit={{ opacity: 0, x: 10 }}
+                                    transition={{ duration: 0.2, ease: "easeOut" }}
+                                    className="text-yellow-400 text-md p-3 font-bold font-mono"
+                                >
+                                    No match found
+                                </motion.li>
+                            )}
                         </ul>
                     </motion.div>
-                ) }
-                { errorState && (
+                )}
+
+                {/* Error State */}
+                {errorState && (
                     <motion.div
                         id="errorState"
                         className="absolute w-full mt-2 bg-red-800 bg-opacity-90 text-white px-4 py-2 rounded-lg"
-                        initial={ { opacity: 0, y: -10 } }
-                        animate={ { opacity: 1, y: 0 } }
-                        exit={ { opacity: 0, y: -10 } }
-                        transition={ { duration: 0.3, ease: "easeOut" } }
+                        initial={{ opacity: 0, y: -10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -10 }}
+                        transition={{ duration: 0.3, ease: "easeOut" }}
                     >
-                        { errorState }
+                        {errorState}
                     </motion.div>
-                ) }
+                )}
             </AnimatePresence>
         </div>
     );
-};
+}

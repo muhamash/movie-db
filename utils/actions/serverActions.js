68196a2fabcbbda8d2ab8178302d2732e4810933
muhamsh <muhammad-ashraful@outlook.com>
2024-12-22 00:15:25 +0600

    test prod

--- utils/actions/serverActions.js
+++ utils/actions/serverActions.js
@@ -1,17 +1,20 @@
 'use server';
 
 export async function handleMovieClick( movieId, auth ) {
-  return `/movie/${movieId}?userId=${auth?.id === undefined ? 'notLoggedIn' : auth?.id}`;
+  // return `/movie/${movieId}?userId=${auth?.id === undefined ? 'notLoggedIn' : auth?.id}`;
+
+  return `https://movie-db-eight-sable.vercel.app/movie/${movieId}?userId=${auth?.id === undefined ? 'notLoggedIn' : auth?.id}`;
 };
 
 export async function handleWatchListClick ( userId )
 { 
   if (userId) {
-    return `/watchList/${userId}`;
+    // return `/watchList/${userId}`;
+    return `https://movie-db-eight-sable.vercel.app/watchList/${userId}`;
   }
 
   else {
-    return `/login`;
+    // return `/login`;
+    return `https://movie-db-eight-sable.vercel.app/login`;
   }
 };
-

92278aa6e5b41b10d10b1e49f70fd540f47a6fce
muhamsh <muhammad-ashraful@outlook.com>
2024-12-23 15:56:29 +0600

    not found and loading page style fix

--- app/compare/@slotId/loading.js
+++ app/compare/@slotId/loading.js
@@ -1,6 +1,6 @@
 export default async function loading() {
   return (
-    <div className="w-full h-full flex flex-col gap-5 items-center justify-center">
+    <div className="w-full py-20 h-full flex flex-col gap-5 items-center justify-center">
       <div className="pageLoader"></div>
     </div>
   )
--- app/compare/@slotId/not-found.js
+++ app/compare/@slotId/not-found.js
@@ -1,6 +1,6 @@
 export default async function notFound() {
   return (
-    <div className='w-full h-full flex flex-col gap-5 items-center justify-center text-md text-red-600'>
+    <div className='w-full h-full flex flex-col gap-5 items-center justify-center text-md text-red-600 py-20'>
       <div className='notFoundLoader'></div>
       <p className='text-center'>Not Found</p>
     </div>
